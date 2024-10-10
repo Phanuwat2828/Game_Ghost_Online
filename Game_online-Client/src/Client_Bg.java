2a73b1931860e66f6f8f376365b1baa19ece1002
jaojuiii <jaojuigraphic2@gmail.com>
2024-10-10 15:53:40 +0700

    Update Random zombie On Frame

--- Game_online-Client/src/Client_Bg.java
+++ Game_online-Client/src/Client_Bg.java
@@ -1,3 +1,8 @@
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
+
 import javax.swing.JFrame;
 
 public class Client_Bg extends JFrame{
@@ -5,7 +10,6 @@ public class Client_Bg extends JFrame{
         Client_Bg bg = new Client_Bg();
         Client_Jpanel panel = new Client_Jpanel();
         bg.add(panel);
-        panel.test();
         bg.setVisible(true);
     }
     
@@ -14,6 +18,20 @@ public class Client_Bg extends JFrame{
         setSize(1920, 1080 );
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        addMouseMotionListener(new MouseMotionListener() {
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'mouseDragged'");
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+              setTitle("x :"+e.getX()+" "+"Y :"+e.getY());
+            }
+            
+        });
     }
     
 }
--- Game_online-Client/src/Client_Jpanel.java
+++ Game_online-Client/src/Client_Jpanel.java
@@ -1,23 +1,37 @@
 import java.io.File;
+import java.util.Random;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import javax.swing.JPanel;
 
 public class Client_Jpanel extends JPanel {
-    String path =  System.getProperty("user.dir")+File.separator +"Game_online-Client"+ File.separator + "src"+ File.separator + "Image";
-    Image image_bg =Toolkit.getDefaultToolkit().createImage(path+ File.separator + "Background.png");
+    String path_Bg =  System.getProperty("user.dir")+File.separator +"Game_online-Client"+ File.separator + "src"+ File.separator + "Image";
+    Image image_bg =Toolkit.getDefaultToolkit().createImage(path_Bg+ File.separator + "Background.png");
+
+    String path_gif = System.getProperty("user.dir")+File.separator +"Game_online-Client"+ File.separator + "src"+ File.separator + "Gif";
+    Image image_gif =Toolkit.getDefaultToolkit().createImage(path_gif+ File.separator + "Zombie_walk.gif");
+    int [] axisX = new int[5];
+    int [] axisY = new int[5];
+
+    Random rand = new Random();
+
     public Client_Jpanel(){
         setSize(1920, 1080);
+        Defualt_Zombie();
+    }
+
+    public void Defualt_Zombie(){
+        for(int i=0; i<5;i++){
+            axisX[i] = rand.nextInt(60,400);
+            axisY[i] = rand.nextInt(200,400); 
+        }
     }
 
     protected void paintComponent(Graphics g){
         g.drawImage(image_bg,0,0,1555,855,this);
-    }
-
-    public void test(){
-        System.out.println("Client_Jpanel");
-         System.out.println(path);
-
+        for(int i=0; i<5;i++){
+            g.drawImage(image_gif,axisX[i],axisY[i],100,100,this);
+        }
     }
 }
