223ab2c0773f49ed95eefd098b5cfcdadb2c1fa7
jaojuiii <jaojuigraphic2@gmail.com>
2024-10-10 17:11:46 +0700

    Update Movement

--- Game_online-Client/src/Client_Jpanel.java
+++ Game_online-Client/src/Client_Jpanel.java
@@ -1,9 +1,14 @@
 import java.io.File;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+
 import javax.swing.JPanel;
+
 
 public class Client_Jpanel extends JPanel {
     String path_Bg =  System.getProperty("user.dir")+File.separator +"Game_online-Client"+ File.separator + "src"+ File.separator + "Image";
@@ -14,21 +19,49 @@ public class Client_Jpanel extends JPanel {
     Random rand = new Random();
     int [] axisX = new int[30];
     int [] axisY = new int[30];
-
+    int [] speedX = new int[30];
+    Timer timer;
 
     public Client_Jpanel(){
         setSize(1920, 1080);
         Defualt_Zombie();
+        startZombieMovement();
     }
 
     public void Defualt_Zombie(){
         for(int i=0; i<30;i++){
             axisX[i] = rand.nextInt(20,419);
             axisY[i] = rand.nextInt(250,650); 
+            speedX[i] = rand.nextInt(1, 5);
         }
     }
 
+
+    public void startZombieMovement() {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                moveZombies();
+                repaint();
+            }
+        }, 0, 50); 
+    }
+
+    public void moveZombies() {
+        for (int i = 0; i < 30; i++) {
+            axisX[i] += speedX[i];
+
+            if (axisX[i] > getWidth()) {
+                axisX[i] = -100; 
+            }
+        }
+    }
+
+
+
     protected void paintComponent(Graphics g){
+        super.paintComponent(g);
         g.drawImage(image_bg,0,0,1555,855,this);
         for(int i=0; i<30;i++){
             g.drawImage(image_gif,axisX[i],axisY[i],100,100,this);
