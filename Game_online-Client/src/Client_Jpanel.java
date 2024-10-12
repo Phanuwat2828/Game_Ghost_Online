515105144d318137215bd9610ba57ccc8bf02c1e
jaojuiii <jaojuigraphic2@gmail.com>
2024-10-13 01:56:22 +0700

    update zombie walk referance lekie check
    ทำให้ผีที่มีความเร็วช้า ภาพเคลื่อนไหวก็จะช้า ถ้าผีมีความเร็ว เร็ว ภาพเคลื่อนไหวก็จะเร็วตาม

--- Game_online-Client/src/Client_Jpanel.java
+++ Game_online-Client/src/Client_Jpanel.java
@@ -6,7 +6,7 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
-
+import java.awt.MediaTracker;
 import javax.swing.JPanel;
 
 
@@ -16,16 +16,35 @@ public class Client_Jpanel extends JPanel {
 
     String path_gif = System.getProperty("user.dir")+File.separator +"Game_online-Client"+ File.separator + "src"+ File.separator + "Gif";
     Image image_gif =Toolkit.getDefaultToolkit().createImage(path_gif+ File.separator + "Zombie_walk.gif");
+
     Random rand = new Random();
     int [] axisX = new int[30];
     int [] axisY = new int[30];
     int [] speedX = new int[30];
+    Image [] zombie_action_walk = new Image[10];
     Timer timer;
+
+// เพิ่มตัวแปร MediaTracker
+    MediaTracker tracker = new MediaTracker(this);
 
     public Client_Jpanel(){
         setSize(1920, 1080);
         Defualt_Zombie();
+        img_zombie_walk();
         startZombieMovement();
+    }
+
+    public void img_zombie_walk() {
+        for (int k = 0; k < 10; k++) {
+            zombie_action_walk[k] = Toolkit.getDefaultToolkit().createImage(path_gif + File.separator + "Zombie_walk" + (k + 1) + ".png");
+            tracker.addImage(zombie_action_walk[k], k);  // เพิ่มภาพเข้าไปใน tracker
+        }
+        
+        try {
+            tracker.waitForAll();  // รอให้โหลดภาพทั้งหมดก่อน
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void Defualt_Zombie(){
@@ -58,13 +77,17 @@ public class Client_Jpanel extends JPanel {
         }
     }
 
-
-
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawImage(image_bg,0,0,1555,855,this);
-        for(int i=0; i<30;i++){
-            g.drawImage(image_gif,axisX[i],axisY[i],100,100,this);
+        g.drawImage(image_bg, 0, 0, 1555, 855, this);
+    
+        // วาดภาพซอมบี้
+        for(int i = 0; i < 30; i++){
+            // ยิ่งซอมบี้ช้ามาก ตัวคูณของ frameDelay จะยิ่งมาก ทำให้เปลี่ยนเฟรมช้าลง
+            int frameDelay = 500 / speedX[i]; // กำหนดความเร็วในการเปลี่ยนเฟรมตามความเร็วของซอมบี้
+            int frame = (int) ((System.currentTimeMillis() / frameDelay) % 10); // คำนวณเฟรมตามความเร็วที่ปรับแล้ว
+            
+            g.drawImage(zombie_action_walk[frame], axisX[i], axisY[i], 100, 100, this);
         }
     }
 }
